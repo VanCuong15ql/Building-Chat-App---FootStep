b9d45a0d48a1e857b7cacd721bf939431b4641e5
congktx <buicongvip@gmail.com>
2024-11-25 10:52:43 +0700

    fix socket port

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client"
 let socket;
 
 const connectSocket = (user_id) => {
-    socket = io("http://localhost:3001", {
+    socket = io("http://localhost:4000", {
         query: `user_id=${user_id}`
     })
 }
